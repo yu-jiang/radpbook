8a8bc399869d95f5ef7cb819b556382f839db040
Yu Jiang <yjiang26@gmail.com>
2015-08-31 00:55:37 -0400

    making OOP version of Chapter 8 simulations

--- Chapter8_Ex_ADP/MovementSimulator.m
+++ Chapter8_Ex_ADP/MovementSimulator.m
@@ -1,80 +1,212 @@
 classdef MovementSimulator < handle
     
     properties
-        tau = 0.05; % (s)
-        m1 = 2;        % kg
-        m2 = 2;        % kg
+        tau = 0.05;
+        m1 = 2;
+        m2 = 2;
         d11 = 0;
-        d12 = 0;
-        d21 = 0;
+        d12 = 13;
+        d21 = -13;
         d22 = 0;
+        
         c1 = 0.15/2;
         c2 = 0.05/2;
         dt = 0.005;
-%         
-%         in = 0;
-%         
-        A0 = [0 0 1        0      0      0;
-              0 0 0        1      0      0;
-              0 0 0        0     1/m1    0;
-              0 0 0        0      0      1/m2;
-              0 0 0        0      -1/tau 0;
-              0 0 0        0      0      -1/tau];
         
-        A = A0+[0         0 0     0      0 0;
-            0         0 0     0      0 0;
-            150/m1    0 0     0      0 0;
-            0         0 0     0      0 0;
-            0         0 0     0      0 0;
-            0         0 0     0      0 0];
-
-        B = [0 0;
-            0 0;
-            0 0;
-            0 0;
-            1/tau 0;
-            0 1/tau];
+        in = 0;
         
-        Q0 = diag([500 1000]);  % weighting matrix
-        R = diag([0.01,0.01]);  % weighting matrix 
-        theta = 0*15/180*pi;    %0.9453;
-        Qc = Q0;
-        Q = blkdiag(Qc,0.01*Qc,0.00005*Qc);
-
-        theta1 = 0/180*pi;%0.9453;
-        TM1 = [cos(theta1) -sin(theta1); sin(theta1) cos(theta1)];
-%         %Qc1 = Q0+ 250*[log(1+150) 0; 0 0];
-%         Qc1 = Q0+ 1e4*[.03 0; 0 0];
-%         Q1 = blkdiag(Qc1,0.01*Qc1,0.00005*Qc1);
-       R = TM1'*R*TM1;
-%         
-%         %Q=10*diag([50,100,2,2,0.001,0.001]);R=diag([.01,.01]);
-%         %Q1=10*diag([150,100,2,2,0.001,0.001]);
-%         
-%         K=lqr(A0,B,Q,R);
-%         
-%         Ko=lqr(A,B,Q1,R);
-%         K0=K;
-%         K0(1,1)=K(1,1)+350;%230;
-%         K0(2,2)=K(1,1);
-%         
-%         
-%         %K=K1
-%         %eig(A-B*K);
+        A0
         
+        A
+        
+        B
+        
+        Q0 = [500 0; 0 1000];
+        Q;
+        R = diag([0.01,0.01]);
+        
+        
+        
+        theta=0*15/180*pi;%0.9453;
+        
+        
+        theta1=0/180*pi;%0.9453;
+        
+        K
+        Ko
+        K0
     end
     
+    % Public methods for interfacing
     methods
-        function this = MovementSimulator(fieldDynamics)
+        function this = MovementSimulator(this)
             Initialize(this);
+        end
+        
+        % Simulation for the NF
+        function SimNF(this)
+            [t,y1]=ode_yu_NF(0,.7,[0.001*(rand-0.5),-.25,0,0,0,0,zeros(1,12+1+4)]',this.dt, this);
+            [t,y2]=ode_yu_NF(0,.7,[0.001*(rand-0.5),-.25,0,0,0,0,zeros(1,12+1+4)]',this.dt, this);
+            [t,y3]=ode_yu_NF(0,.7,[0.001*(rand-0.5),-.25,0,0,0,0,zeros(1,12+1+4)]',this.dt, this);
+            [t,y4]=ode_yu_NF(0,.7,[0.001*(rand-0.5),-.25,0,0,0,0,zeros(1,12+1+4)]',this.dt, this);
+            [t,y5]=ode_yu_NF(0,.7,[0.001*(rand-0.5),-.25,0,0,0,0,zeros(1,12+1+4)]',this.dt, this);
             
+            figure(1)
+            subplot(141)
+            fCirc([0,0],0.0075,1000,'r')
+            hold on
+            plot(y1(:,1),y1(:,2), 'b-', ...
+                y2(:,1),y2(:,2), 'b-', ...
+                y3(:,1),y3(:,2), 'b-', ...
+                y4(:,1),y4(:,2), 'b-', ...
+                y5(:,1),y5(:,2), 'b-')
+            hold off
+            axis equal
+            axis([-0.05 0.05 -0.3 .05])
+            xlabel('x-position (m)')
+            ylabel('y-position (m)')
+            title('A')
+            
+            
+            figure(4)
+            subplot(441)
+            plot(t,y1(:,3),'b-', ...
+                t,y2(:,3),'b-', ...
+                t,y3(:,3),'b-', ...
+                t,y4(:,3),'b-', ...
+                t,y5(:,3),'b-')
+            xlabel('time (s)')
+            ylabel('x-velocity (m/s)')
+            title('A')
+            axis([0 0.6 -0.5 0.5])
+            subplot(445)
+            plot(t,y1(:,4),'b-', ...
+                t,y2(:,4),'b-', ...
+                t,y3(:,4),'b-', ...
+                t,y4(:,4),'b-', ...
+                t,y5(:,4),'b-')
+            
+            axis([0 0.6 -.5 1.5])
+            xlabel('time (s)')
+            ylabel('y-velocity (m/s)')
+            
+            subplot(4,4,9)
+            plot(t,y1(:,5),'b-', ...
+                t,y2(:,5),'b-', ...
+                t,y3(:,5),'b-', ...
+                t,y4(:,5),'b-', ...
+                t,y5(:,5),'b-')
+            xlabel('time (s)')
+            ylabel('x-endpoint force (N)')
+            axis([0 0.6 -15 15])
+            
+            
+            subplot(4,4,13)
+            plot(t,y1(:,6),'b-', ...
+                t,y2(:,6),'b-', ...
+                t,y3(:,6),'b-', ...
+                t,y4(:,6),'b-', ...
+                t,y5(:,6),'b-')
+            
+            axis([0 0.6 -30 30])
+            xlabel('time (s)')
+            ylabel('y-endpoint force (N)')
+            
+            toc
         end
         
-        function this = Initialize(this)
-        end
-        
-        function make_a_movement(this)
-        end
-        
+        %         function SimDF
+        %         end
+        %
+        %         function SimAE
+        %         end
     end
+    
+    % Private methods for internal use
+    methods (Access = private)
+        function Initialize(this)
+            
+            this.A0 = [0 0 1        0      0      0;
+                0 0 0        1      0      0;
+                0 0 0        0     1/this.m1    0;
+                0 0 0        0      0      1/this.m2;
+                0 0 0        0      -1/this.tau 0;
+                0 0 0        0      0      -1/this.tau];
+            
+            this.A = this.A0 + [zeros(2,6);
+                150/this.m1  zeros(1,5);
+                zeros(3,6)];
+            
+            this.B=[0 0;
+                0 0;
+                0 0;
+                0 0;
+                1/this.tau 0;
+                0 1/this.tau];
+            
+            Qc = this.Q0;
+            this.Q = blkdiag(Qc,0.01*Qc,0.00005*Qc);
+            
+            TM1 = [cos(this.theta1) -sin(this.theta1);
+                sin(this.theta1) cos(this.theta1)];
+            
+            Qc1 = this.Q0+ 1e4*[.03 0; 0 0];
+            Q1 = blkdiag(Qc1,0.01*Qc1,0.00005*Qc1);
+            this.R = TM1'*this.R*TM1;
+            
+            this.K = lqr(this.A0, this.B, this.Q, this.R);
+            
+            this.Ko = lqr(this.A,this.B,Q1,this.R);
+            this.K0 = this.K;
+            this.K0(1,1) = this.K(1,1) + 350;%230;
+            this.K0(2,2) = this.K(1,1);
+        end
+    end
+end
+
+%%  ================= Local Functions ========================== %
+
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% LocalFunctions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [t,Y] = ode_yu_NF(t0,tf,x0,dt,this)
+h = dt;
+t = t0:dt:tf;
+y = x0;
+Y = [];
+for clock = t
+    y = y + motor2DNF(y,this.A0,this.B,this.Q,this.R, ...
+        this.c1,this.c2,this.K,h)*h;
+    Y = [Y y];
+end
+Y = Y';
+end
+
+function dX = motor2DNF(X,A0,B,Q,R,c1,c2,K,dt)
+x = X(1:6);
+u = -K*x;
+w = randn(2,1)*sqrt(dt);
+M = [c1*u(1) c2*u(2); 
+     -c2*u(1) c1*u(2)];
+v = M*w; % control dependent noise
+dx = A0*x + B*u + B*v./dt; %6
+dIq = x'*(Q+K'*R*K)*x; %1
+dxxi = kron(x',v'*R)'./dt; %12;
+duu = kron(v,v)./dt;         % 4
+dX =[dx;  % \dot x
+    dIq; % reward
+    dxxi;
+    duu];
+end
+
+% ========
+function h = fCirc(center,r,N,color)
+theta = linspace(0,2*pi,N);
+rho = ones(1,N)*r;
+[X,Y] = pol2cart(theta,rho);
+X = X+center(1);
+Y = Y+center(2);
+h = fill(X,Y,color);
+axis square;
 end
