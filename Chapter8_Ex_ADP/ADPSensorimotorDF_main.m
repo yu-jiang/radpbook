2ff33ef0c223baf3b718840a92a6e96c895ee595
Yu Jiang <yu.jiang@nyu.edu>
2015-09-11 23:51:31 -0400

    evening job

--- Chapter8_Ex_ADP/ADPSensorimotorDF_main.m
+++ Chapter8_Ex_ADP/ADPSensorimotorDF_main.m
@@ -1,7 +1,15 @@
-close all
+close all; clear
+%% Simulate the DF
 M = DFSimulator();
 simNF(M);
 simDF(M);
+simAL(M);
+simAE(M);
+showStiffness(M);
+%% Simulate the VF
+M = VFSimulator();
+simNF(M);
+simVF(M);
 simAL(M);
 simAE(M);
 showStiffness(M);
