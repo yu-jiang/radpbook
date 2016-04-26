e96dc97d81dc4a1ea1faa6b8e8503b073b7e9780
Yu Jiang <yjiang26@gmail.com>
2016-04-26 00:16:02 -0400

    add version checking for simulink model

--- Chapter2_Example1/Ch2Ex1_main.m
+++ Chapter2_Example1/Ch2Ex1_main.m
@@ -5,10 +5,19 @@
 % learns the optimal performance via real-time data.
 %
 % Copyright 2016 Yu Jiang
-mdl = 'LinearADPSimullink_R2014b';
+
+%% Check your MATLAB version
+mlver = ver('MATLAB');
+rlStr = mlver.Release(5:7); % Get release number
+if ismember(rlStr, {'14b', '15a', '15b'});
+   mdl = ['LinearADPSimullink_R20', rlStr];
+else
+   error('The example is only supported in MATLAB R2014b--R2015b')
+end
+
 %% Open the model
-%
 open_system(mdl);
+
 %% Simulate the system
 %
 sim(mdl);
